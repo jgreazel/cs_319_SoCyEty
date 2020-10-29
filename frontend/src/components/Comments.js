0fb3401cd1d482b3b6d5440d2251dd1b409e4c57
XinJun <xinjun99@iastate.edu>
2020-10-29 00:25:50 -0500

    added post comment box

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -4,7 +4,32 @@ class Comments extends React.Component {
     render() {
         return (
             <div class="column side">
-                <h2>Comments on solution.</h2>
+                <h2>Discussion</h2>
+                <nav>
+                    <ul>
+                        <li>Comment 1</li>
+                        <li>Comment 2</li>
+                        <li>Comment 3</li>
+                        <li>Comment 4</li>
+                        <li>Comment 5</li>
+                        <li>Comment 6</li>
+                        <li>Comment 7</li>
+                        <li>Comment 8</li>
+                        <li>Comment 9</li>
+                        <li>Comment 10</li>
+                        <li>Comment 11</li>
+                        <li>Comment 12</li>
+                        <li>Comment 13</li>
+                        <li>Comment 14</li>
+                        <li>Comment 15</li>
+                        <li>Comment 16</li>
+                    </ul>
+                </nav>
+                <form onSubmit="return false;">
+                    <textarea class="comment" placeholder="Type your comments here"></textarea>
+                    <br/>
+                    <input type="submit" value="Submit"/>
+                </form>
             </div>
         );
     }
