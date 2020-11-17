4ac2740e1184d04c21fcaacc68d751982080f6c9
Jon <jdgreazel@gmail.com>
2020-11-16 21:12:16 -0600

    left a comment

--- frontend/src/components/Solution.js
+++ frontend/src/components/Solution.js
@@ -20,7 +20,10 @@ class Solution extends React.Component {
             <div >  
                 <h2 className="posts_heading">Question Solutions:</h2>
                 {solution.solution && solution.solution.map(s => 
-                    <SolutionCard key={s.id} data={s} />
+                    <div>
+                        <SolutionCard key={s.id} data={s} />
+                        {/* Comments.js will go here, make sure to pass in id={s.id} */}
+                    </div>
                 )}
             </div>
         );
